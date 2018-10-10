288668213d1d23afc9fae0280f8b02c6de8b8f69
1003yd <1003yd@gmail.com>
2018-10-10 17:55:29 +0900

    second commit

--- src/main/java/kr/or/ddit/web/BasicServlet.java
+++ src/main/java/kr/or/ddit/web/BasicServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+//주석 변경
 @WebServlet("/BasicServlet")
 public class BasicServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
